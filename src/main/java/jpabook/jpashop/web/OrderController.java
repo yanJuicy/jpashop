68969689142a45180699e7f986f4e8cb4ab831be
yanJuicy <hanjub3@gmail.com>
2023-04-22 12:26:52 +0900

    주문 취소

--- src/main/java/jpabook/jpashop/web/OrderController.java
+++ src/main/java/jpabook/jpashop/web/OrderController.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.web;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
@@ -9,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -40,4 +43,11 @@ public class OrderController {
         return "redirect:/orders";
     }
 
+    @GetMapping(value = "/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+
+        return "order/orderList";
+    }
 }
